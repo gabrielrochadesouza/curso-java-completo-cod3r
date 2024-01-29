dd5385de162bdefefd80d66f32259e8270a54edd
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-01-29 15:28:03 -0300

    Módulo Fundamentos
    Hoje reassisti alguns vídeos do curso e fiz atividade de calculo.

---
+++ Módulo Fundamentos/Fundamentos/src/Fundamentos/Teste.java
@@ -0,0 +1,17 @@
+package Fundamentos;
+
+import java.util.Scanner;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		System.out.println("Olá terminal do computador!");
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String valor = entrada.nextLine();
+		
+		System.out.println("Valor é "  + valor);
+		entrada.close();
+	}
+}
