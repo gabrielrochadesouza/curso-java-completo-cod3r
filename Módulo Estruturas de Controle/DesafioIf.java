cbe78aaaebacb83fa35c99049270e8cf1e93e0ee
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-01-31 16:33:37 -0300

    Módulo Estruturas De Controle
    Aprendi sobre o If e que Estruturas De Controle não vai ;

---
+++ Módulo Estruturas de Controle/DesafioIf.java
@@ -0,0 +1,17 @@
+package Controle;
+
+public class DesafioIf {
+	public static void main(String[] args) {
+		
+		double nota = 1.3;
+		
+		// não usar; em estruturas de controle (tem uma exceção) 	
+		if (nota >= 9) {
+		System.out.println("Quadro de Honra!");
+		System.out.println("Voce é fera!!!");
+			
+	
+			
+		}
+	}
+}
