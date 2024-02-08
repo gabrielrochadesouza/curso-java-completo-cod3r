f6c08b28c2950773419623c4a790b8349e5458f9
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-02-08 16:49:44 -0300

    Pratica
    Revi e comentei.

---
+++ Pratica/Import.java
@@ -0,0 +1,19 @@
+package Pratica1;
+
+import java.util.Date;
+
+public class Import {
+	
+	public static void main(String[] args) {
+	
+	java.lang.String b = "Boa tarde";
+	java.lang.System.out.println(b);
+	
+	String s = "Boa noite!";
+	System.out.println(s);
+	
+	// Date mostra a data atual, tem que reportar java.util.Date;
+	Date d = new Date(); 
+	System.out.println(d);
+	}
+}
---
+++ Pratica/Inferência.java
@@ -0,0 +1,29 @@
+package Pratica1;
+
+public class Inferência {
+
+	public static void main(String[] args) {
+		
+		double a = 5.5; // declaradado e iniciado
+		System.out.println(a);
+		
+		var b = 10.53;
+		System.out.println(b);
+		
+		var c = "texto"; // da para escrever com var mas tem que usar ""
+		System.out.println(c);
+		
+		c = "outro texto"; 
+		System.out.println(c);
+		
+		double d; // variavel foi declarado
+		d = 13.45; // variavel foi iniciado
+		System.out.println(d); // usado!
+		
+		var e = 15.55;
+		System.out.println(e);
+	
+		var f = 13; // inteiro
+		System.out.println(f);
+	}
+}
