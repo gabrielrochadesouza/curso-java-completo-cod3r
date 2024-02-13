5fee0a2dde34d028f208929caccbfc8f626dcb02
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-02-12 21:16:03 -0300

    Módulo Estruturas de Controle
    Revi Alguns vídeos e aprendi sobre o switch, case, for

---
+++ Módulo Estruturas de Controle/for3.java
@@ -0,0 +1,17 @@
+package Controle;
+
+public class for3 {
+	
+	public static void main(String[] args) {
+		
+		
+		 for(int i = 1; i< 10; i++) {
+		    for(int j = 0; j < 10; j++) {
+		   System.out.printf("[%d %d]", i,j); /* printf serve para colocar simbolo 
+		   no valor de número ou palavra
+      		*/
+       } 
+	  System.out.println();
+	    }
+	  }
+    }
