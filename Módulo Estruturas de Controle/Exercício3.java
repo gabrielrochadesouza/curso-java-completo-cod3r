71b939ee9df35ed6e443c820755ed51139e21cff
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-02-14 12:06:31 -0300

    Modulo Estruturas de Controle
    Revi alguns vídeos e fiz 2 exercícios de fim de Módulo

---
+++ Módulo Estruturas de Controle/Exercício3.java
@@ -0,0 +1,38 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class Exercício3 {
+
+	/**
+	 * 3. Criar um programa que receba duas notas parciais, calcular a média
+	 * final. Se a nota do aluno for maior ou igual a 7.0 imprime no console
+	 * "Aprovado", se a nota for menor que 7.0 e maior do que 4.0 imprime no console
+	 * "Recuperação", caso contrário imprime no console "Reprovado".
+	 */
+	
+	public static void main(String[] args) {
+		
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.println("Nota Parcial 1");
+	double nota1 = scanner.nextDouble();
+	
+	System.out.println("Nota Parcial 2");
+	double nota2 = scanner.nextDouble();
+	
+	double media = (nota1 + nota2) / 2;
+	
+	if(media >=7) {
+		System.out.println("Sua média foi " + media + " e você esta aprovado");
+	
+		
+	}else if (media >= 4){
+		System.out.println("Sua média foi" + media + "e você esta em recuperação");
+	} else {
+		System.out.println("Sua média foi" + media + "você está reprovado");
+	}
+	
+	scanner.close();	
+	}
+}
