9b64c38caf8648956745017ffd22a031a60f6161
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-03-19 20:48:30 -0300

    Pratica
    Refiz o que eu tinha feito e assisti alguns
    e foquei mais na teoria

---
+++ Pratica/treino0.java
@@ -0,0 +1,30 @@
+package Pratica1;
+
+public class treino0 {
+
+public static void main(String[] args) {
+		
+		double a = 5.5; // declaradado e iniciado
+		System.out.println(a);
+		
+		var b = 10.53;
+		System.out.println(b);
+		
+		var c = "NÓIS"; // da para escrever com var mas tem que usar ""
+		System.out.println(c);
+		
+		c = "Gente Boa"; 
+		System.out.println(c);
+		
+		double d; // variavel foi declarado
+		d = 13.45; // variavel foi iniciado
+		System.out.println(d); // usado!
+		
+		var e = 15.55;
+		System.out.println(e);
+	
+		var f = 13; // inteiro
+		System.out.println(f);
+	}
+}
+
