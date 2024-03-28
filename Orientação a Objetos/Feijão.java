16c4390d2157c7ebb97f8b96618d7e73b49fdf12
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2024-03-27 23:57:57 -0300

    Orientação a Objetos
    Fiz Polimorfismo não sei muito o que é nem o getters nem o setters .

---
+++ Orientação a Objetos/Feijão.java
@@ -0,0 +1,9 @@
+package Polimorfismo;
+
+public class Feijão extends Comida {
+
+	public Feijão(double peso) {
+		super(peso);
+	}	
+	 }
+
---
+++ Orientação a Objetos/Jantar.java
@@ -0,0 +1,27 @@
+package Polimorfismo;
+
+public class Jantar {
+
+	public static void main(String[] args) {
+		
+		Pessoa convidado = new Pessoa(99.55);
+	
+		
+		Arroz ingrediente1 = new Arroz(0.20);
+		Feijão ingrediente2 = new Feijão(0.19);
+	
+		System.out.println(convidado.getPeso());
+	
+		convidado.comer(ingrediente1);
+		convidado.comer(ingrediente2);
+		System.out.println(convidado.getPeso());
+		
+		Sorvete sobremesa = new Sorvete(0.4);
+		
+		convidado.comer(sobremesa);
+		System.out.println(convidado.getPeso());
+		
+	
+	
+	}
+}
